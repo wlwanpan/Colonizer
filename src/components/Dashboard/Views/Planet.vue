62766f8e5078fba2b2ebdf0f1634f4366c208dfa
Estelle Wan <eccheung@amazon.com>
2018-03-17 14:54:52 -0700

    Some CSS fixes

--- src/components/Dashboard/Views/Planet.vue
+++ src/components/Dashboard/Views/Planet.vue
@@ -366,6 +366,7 @@ body {
 #tooltip-container {
   position: absolute;
   background-color: #fff;
+  opacity: .8;
   color: #000;
   padding: 10px;
   border: 1px solid;
@@ -391,7 +392,7 @@ body {
   margin: 0 auto; /* Gives that scrolling effect as the typing happens */
   letter-spacing: .15em; /* Adjust as needed */
   animation:
-    typing 3.5s steps(30, end),
+    typing 2s steps(30, end),
     blink-caret .5s step-end infinite;
 }
 
@@ -407,6 +408,9 @@ body {
   50% { border-color: orange }
 }
 
+.card {
+  opacity: .9;
+}
 .card.card-stats:hover {
     background-color: #dddddd;
     cursor: pointer;
@@ -414,12 +418,12 @@ body {
 }
 .main-panel {
   background-color:black;
-background-image:
-radial-gradient(white, rgba(255,255,255,.2) 2px, transparent 40px),
-radial-gradient(white, rgba(255,255,255,.15) 1px, transparent 30px),
-radial-gradient(white, rgba(255,255,255,.1) 2px, transparent 40px),
-radial-gradient(rgba(255,255,255,.4), rgba(255,255,255,.1) 2px, transparent 30px);
-background-size: 550px 550px, 350px 350px, 250px 250px, 150px 150px;
-background-position: 0 0, 40px 60px, 130px 270px, 70px 100px;
+  background-image:
+  radial-gradient(white, rgba(255,255,255,.2) 2px, transparent 40px),
+  radial-gradient(white, rgba(255,255,255,.15) 1px, transparent 30px),
+  radial-gradient(white, rgba(255,255,255,.1) 2px, transparent 40px),
+  radial-gradient(rgba(255,255,255,.4), rgba(255,255,255,.1) 2px, transparent 30px);
+  background-size: 550px 550px, 350px 350px, 250px 250px, 150px 150px;
+  background-position: 0 0, 40px 60px, 130px 270px, 70px 100px;
 }
 </style>
