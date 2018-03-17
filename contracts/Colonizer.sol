0e9380d8d0f8dd8964d7db4777936641b8833ad2
Warren <wlwanpan@uwaterloo.ca>
2018-03-16 19:54:47 -0700

    added details to interface

--- contracts/Colonizer.sol
+++ contracts/Colonizer.sol
@@ -1,12 +1,18 @@
 
 contract Colonizer {
 
+  enum habitantState { online, offline, archieved }
+
   struct Habitant {
 
     string fullName;
+    string username;
+    habitantState status;
     bytes32 passwordhash;
     string colony;
+
     address[] assets;
+    address[] relatives;
 
   }
 
@@ -22,17 +28,26 @@ contract Colonizer {
   mapping (address => Asset) assets; // store assets registered
   mapping (address => Habitant) habitants; // store user by address
 
-  function registerHabitant () {
+  function registerHabitant (string fullname, string username, string password, string colony) {
     // register an account with from using struct above
+    msg.sender = address = Habitant()
+
+    habitants[msg.sender] = Habitant()
+
   }
 
-  function loginHabitant () {
+  function loginHabitant (string username, string password) {
     // login account and increment online user count
+    bytes hash = keccab(username, password);
 
   }
 
   function logoutHabitant () {
     // louout and decrement online user count
+
+  }
+
+  function deRegister () {
 
   }
 
