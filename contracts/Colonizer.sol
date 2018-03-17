05d7236e2cb733cb8a471dc89ecff0313e5269a1
Warren <wlwanpan@uwaterloo.ca>
2018-03-16 19:42:03 -0700

    added details

--- contracts/Colonizer.sol
+++ contracts/Colonizer.sol
@@ -10,19 +10,29 @@ contract Colonizer {
 
   }
 
+  struct Asset {
 
-  mapping (address => Habitant) habitants;
+    string description;
+    string type;
+    bytes32 longitude;
+    bytes32 latitude;
 
+  }
+
+  mapping (address => Asset) assets; // store assets registered
+  mapping (address => Habitant) habitants; // store user by address
 
   function registerHabitant () {
-    // register an account with from abocve ^
+    // register an account with from using struct above
   }
 
   function loginHabitant () {
+    // login account and increment online user count
 
   }
 
   function logoutHabitant () {
+    // louout and decrement online user count
 
   }
 
